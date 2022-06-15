275e5af1795bb871c9a1ee72f1c74c776b95aac7
Регина Васильева <skafest123@gmail.com>
2022-06-15 17:25:54 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int one_mile_rub = 20;
+
+        int price = 5000;
+        int bonus = price / one_mile_rub;
+
+        System.out.println("bonus:" + bonus);
+
+    }
+}
