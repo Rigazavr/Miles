b6a3dd4969bbc1db96d20aec50315c17e8d2327e
Регина Васильева <skafest123@gmail.com>
2022-06-24 18:20:14 +0700

    save

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        int one_mile_rub = 20;
+        int oneMileRub = 20;
 
         int price = 5000;
-        int bonus = price / one_mile_rub;
+        int bonus = price / oneMileRub;
 
         System.out.println("bonus:" + bonus);
 
